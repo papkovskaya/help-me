7f0d64e491dfcad5059ce0ca52e01097d7bfca90
Olga Papkovskaya <o.papkovskaya@gmail.com>
2018-03-03 01:32:25 +0300

    just idea, work only for 3 tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
 module.exports = function count(s, pairs) {
-  // your implementation
+  let N = 1;
+  let result = 0;
+  let mass = "";
+  for (let i = 0; i < pairs.length; i++){
+    if (pairs[i][1] < 10){
+      N *= pairs[i][0];
+    } else {
+      return;
+    }
+  }
+
+  let gcd = function (a, b) {
+    if (!b) {
+      return a;
+    }
+
+    return gcd(b, a % b);
+  };
+
+  let n = N + s.length - 1;
+
+  for (let k = 1; k <= n; k++){
+    if (gcd(k, N) == 1) {
+      mass += "1";
+    } else {
+      mass += "0";
+    }
+  }
+
+  for (let k = 0; k < mass.length; k++) {
+    if (mass[k] == s[0]){
+      let temp = k;
+      let check = true;
+      for ( let j = 1; j < s.length; j++){
+        k++;
+        if (mass[k] != s[i]){
+          check = false;
+          break;
+        }
+      }
+
+      if (check == true){
+        result += 1;
+      }
+      k = temp;
+    }
+  }
+
+  for (let i = 0; i < pairs.length; i++) {
+    while (pairs[i][1] > 1) {
+      result *= pairs[i][0];
+      pairs[i][1] -= 1;
+    } 
+  }
+
+  result = result % 1000000007;
+  return result;
 }
+
